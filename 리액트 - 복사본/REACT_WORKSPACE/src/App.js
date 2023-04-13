5c8a4469774e32651e218ad4e25bb23afe86ffff
iiinbo <124110271+iiinbo@users.noreply.github.com>
2023-04-13 17:53:43 +0900

    20230413 today

---
+++ 리액트 - 복사본/REACT_WORKSPACE/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+//
+import Layout from './component/Layout'; // ** 라우터 추가
+import Home from './component/Home'; // ** 라우터 추가
+import BoardList from './component/board/BoardList'; // ** 라우터 추가
+import BoardWrite from './component/board/BoardWrite'; // ** 라우터 추가
+import BoardView from './component/board/BoardView'; // ** 라우터 추가
+import ScoreListFront from './component/score/ScoreListFront'; // ** 실습과제 추가
+//
+function App() {
+  return (
+    <div className="App">
+        <Routes>
+              <Route path='/' element={ <Layout/> } >
+              <Route index element={ <Home/> } />
+              <Route path='/board/List' element={ <BoardList/> } />
+              <Route path='/board/write' element={ <BoardWrite/> } />
+              <Route path='/board/view/:id' element={ <BoardWrite/> } />
+              <Route path='/score/list' element={ <ScoreListFront/> } />
+              </Route>
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
